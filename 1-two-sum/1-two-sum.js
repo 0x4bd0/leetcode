023b3ecb71cd7cd39adbda2b6ab9b9510f732dea
Abdelkader MOUDJAR <abdoumjr@gmail.com>
2022-05-13 15:51:02 +0100

    Time: 69 ms (90.23%), Space: 42.7 MB (52.87%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,10 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = (nums, target) =>  {
-  for( i = 0; i < nums.length-1; i++){
-    for( j = i+1; j < nums.length; j++){
-        if(nums[i] + nums[j] === target) return [i, j]
-  }  
-  }  
+const twoSum =  (nums, target) => {
+  let map = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in map) {
+      return [map[target - nums[i]], i]
+    } 
+      map[nums[i]] = i;
+  }
 };
