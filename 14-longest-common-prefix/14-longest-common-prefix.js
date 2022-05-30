e026ad09dfbccbe2b9e05b340e6fe389c3b1868b
Abdelkader MOUDJAR <abdoumjr@gmail.com>
2022-05-30 21:17:21 +0100

    Time: 90 ms (42.16%), Space: 43.7 MB (25.25%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+const longestCommonPrefix = (strs) => {
+    
+    if(strs.length == 1) return strs.pop()
+    
+    let prefix = ""
+    let checkWord = strs.shift().split('')
+    
+    for(let i = 1; checkWord.length >= i; i++ ){
+        
+        let prefixTest = checkWord.slice(0 , i).join('')
+
+        
+        for(let j = 0; strs.length > j; j++){
+
+            if(prefixTest !== strs[j].slice(0, i)){
+                return prefix
+            }
+            
+        }
+        
+     prefix = prefixTest
+         
+    }
+    
+    return prefix
+}
