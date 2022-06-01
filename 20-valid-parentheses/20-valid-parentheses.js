75984f01657edba91c9a7ce0cc56af85f17d4fbb
Abdelkader MOUDJAR <abdoumjr@gmail.com>
2022-06-01 02:43:43 +0100

    Time: 134 ms (8.15%), Space: 42.1 MB (67.83%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = (s) => {
+    
+    const map = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    }
+    
+    let queue = []
+    
+    for(let i = 0; s.length > i; i++){  
+    (queue.length > 0 && s[i] === map[ queue[queue.length-1] ] ) ? queue.pop() : queue.push(s[i])
+    }
+    
+    return !queue.length
+};
