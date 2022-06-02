1dad72f500b8b2bbbec245758d2e8319ad31c0ce
Abdelkader MOUDJAR <abdoumjr@gmail.com>
2022-06-02 14:27:10 +0100

    Time: 122 ms (14.06%), Space: 44.1 MB (97.19%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+
+const transpose = matrix => {
+    let response = []
+    
+    while(matrix[0].length > 0){
+        response.push( matrix.map( item=>( item.shift() ) ) )
+    }
+    
+    return response
+}
